554222d34356e2e0bfd326a78e53b41f9b93d358
Stolbin <109080579+Stolbin@users.noreply.github.com>
2024-12-19 12:21:14 +0200

    upd

--- js/show-hide_elements.js
+++ js/show-hide_elements.js
@@ -7,12 +7,3 @@ export function hideLoader() {
   const loader = document.getElementById("loader");
   loader.style.display = "none";
 }
-
-const searchInput = document.querySelector(".search_input");
-
-export function showSearchInput() {
-  searchInput.style.display = "block";
-}
-export function hideSearchInput() {
-  searchInput.style.display = "none";
-}
