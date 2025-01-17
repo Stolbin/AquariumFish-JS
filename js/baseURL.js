abdaa6a617cded884444edaa47f61dd6dcf03d00
Stolbin <109080579+Stolbin@users.noreply.github.com>
2025-01-18 00:32:06 +0200

    upd

--- js/baseURL.js
+++ js/baseURL.js
@@ -1,13 +1,7 @@
 const isGitHubPages = window.location.hostname === "stolbin.github.io";
 const basePath = isGitHubPages ? "/AquariumFish/" : "/";
 
-// Додавання <base> тега для коректної роботи відносних шляхів
 const baseElement = document.createElement("base");
 baseElement.setAttribute("href", basePath);
-document.head.appendChild(baseElement);
 
-// Перевірка поточного шляху та перенаправлення лише за потреби
-if (isGitHubPages && !window.location.pathname.startsWith(basePath)) {
-  const redirectTo = basePath + window.location.pathname.replace("/", "");
-  window.location.href = redirectTo;
-}
+document.head.appendChild(baseElement);
