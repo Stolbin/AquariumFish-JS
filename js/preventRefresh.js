57433c9dbcc7eac99597b75ffcd4531df20e5263
Stolbin <109080579+Stolbin@users.noreply.github.com>
2025-01-18 22:42:19 +0200

    Update preventRefresh.js

--- js/preventRefresh.js
+++ js/preventRefresh.js
@@ -1,9 +1,3 @@
-window.addEventListener("beforeunload", function (e) {
-  const message =
-    "Ви хочете покинути сторінку? Всі незбережені зміни будуть втрачені.";
-  return message;
-});
-
 window.addEventListener("keydown", function (event) {
   if (event.key === "F5" || (event.ctrlKey && event.key === "r")) {
     event.preventDefault();
