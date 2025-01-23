25c96d2fe6ca6e72c7b043081b0db68b50e99a9c
Stolbin <109080579+Stolbin@users.noreply.github.com>
2025-01-23 18:26:36 +0200

    upd

--- js/baseURL.js
+++
@@ -1,9 +0,0 @@
-const isGitHubPages = window.location.hostname === "https://stolbin.github.io/";
-const isCorrectPath = window.location.pathname.startsWith("/AquariumFish");
-
-const basePath = isGitHubPages && isCorrectPath ? "/AquariumFish/" : "/";
-
-const baseElement = document.createElement("base");
-baseElement.setAttribute("href", basePath);
-
-document.head.appendChild(baseElement);
