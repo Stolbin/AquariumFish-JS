6fdbfab63ac484f3d03305263998477c213cadf6
Stolbin <109080579+Stolbin@users.noreply.github.com>
2025-04-07 18:18:33 +0300

    upd

--- js/api.js
+++ js/api.js
@@ -1,31 +1,6 @@
-// import { config } from "./config.js";
-
-// export async function fetchFishDataFromAPI() {
-//   try {
-//     const response = await fetch(
-//       `https://api.jsonbin.io/v3/b/${config.BASE_URL}`,
-//       {
-//         headers: {
-//           "X-Master-Key": config.X_MASTERS_KEY,
-//           "X-Access-Key": config.X_ACCESS_KEY,
-//         },
-//       }
-//     );
-//     if (!response.ok) {
-//       throw new Error("Не вдалося завантажити дані");
-//     }
-//     const data = await response.json();
-//     console.log("Дані риб:", data);
-//     return data;
-//   } catch (error) {
-//     console.error("Помилка при завантаженні даних:", error);
-//     throw error;
-//   }
-// }
-
 export async function fetchFishDataFromAPI() {
   try {
-    const BASE_URL = `../data/fish.json`;
+    const BASE_URL = `https://serveraquariumfish.onrender.com/AquariumFish/combinedFishData`;
     const response = await fetch(BASE_URL);
     if (!response.ok) {
       throw new Error("Не вдалося завантажити дані");
