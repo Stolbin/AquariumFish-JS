b16b9124bc69a5acf1a37665e12de578635b97f4
Stolbin <109080579+Stolbin@users.noreply.github.com>
2025-04-24 15:13:26 +0300

    UPD

--- js/backButton.js
+++ js/backButton.js
@@ -1,9 +1,12 @@
 export function createBackButton(onBackClick) {
   const backButton = document.createElement("button");
   backButton.classList.add("back_button");
-  const icon = document.createElement("i");
 
-  backButton.appendChild(icon);
+  const svgArrowBack = document.createElement("img");
+  svgArrowBack.src = "../img/arrow-left2.svg";
+  svgArrowBack.alt = "Arrow Back";
+  svgArrowBack.className = "icon";
+  backButton.appendChild(svgArrowBack);
 
   backButton.addEventListener("click", () => {
     history.replaceState(null, "", "/");
