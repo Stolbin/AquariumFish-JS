06e3c1d407283bafe0b43dbfa0627948df19230b
Stolbin <109080579+Stolbin@users.noreply.github.com>
2025-04-24 18:09:49 +0300

    UPD

--- js/relocationStartNav.js
+++ js/relocationStartNav.js
@@ -1,7 +1,7 @@
 import { clearStateFromStorage } from "./storageFunction.js";
 
 export function relocationStart() {
-  document.querySelector(".nav_dox_link").addEventListener("click", (e) => {
+  document.querySelector(".nav_box_link").addEventListener("click", (e) => {
     e.preventDefault();
     clearStateFromStorage();
     window.location.href = "/";
