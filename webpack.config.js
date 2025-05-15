fe98e2f5772ac9d6bf3fb1d79e83c5cbba5bb357
Stolbin <109080579+Stolbin@users.noreply.github.com>
2025-05-15 14:20:38 +0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,7 @@ module.exports = {
   output: {
     filename: "bundle.[contenthash].js",
     path: path.resolve(__dirname, "dist"),
+    publicPath: "./", // Додаємо тут!
     clean: true,
   },
   module: {
