99210ad81bc508e54d22f34e733799909c18487d
Stolbin <109080579+Stolbin@users.noreply.github.com>
2025-10-31 15:31:40 +0200

    upd

--- src/js/show-hide_elements.js
+++ src/js/show-hide_elements.js
@@ -1,9 +1,11 @@
 export function showLoader() {
   const loader = document.getElementById("loader");
   loader.style.display = "flex";
+  loader.querySelector(".spinner").style.opacity = 1;
 }
 
 export function hideLoader() {
   const loader = document.getElementById("loader");
   loader.style.display = "none";
+  loader.querySelector(".spinner").style.opacity = 0;
 }
