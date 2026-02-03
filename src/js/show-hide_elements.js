07ced28eba278964cb598c7a3984aca0b2c672ca
Stolbin <109080579+Stolbin@users.noreply.github.com>
2026-02-03 18:33:29 +0200

    upd

--- src/js/show-hide_elements.js
+++ src/js/show-hide_elements.js
@@ -1,11 +1,30 @@
 export function showLoader() {
   const loader = document.getElementById("loader");
+  if (!loader) return;
+
+  if (!loader.querySelector(".spinner")) {
+    loader.innerHTML = `
+      <div class="spinner" style="opacity: 0;">
+        <p class="spinner_text">Loading</p>
+        <span></span>
+      </div>
+    `;
+  }
+
+  const spinner = loader.querySelector(".spinner");
   loader.style.display = "flex";
-  loader.querySelector(".spinner").style.opacity = 1;
+
+  requestAnimationFrame(() => {
+    spinner.style.opacity = "1";
+  });
 }
 
 export function hideLoader() {
   const loader = document.getElementById("loader");
+  if (!loader) return;
+
+  const spinner = loader.querySelector(".spinner");
+  if (spinner) spinner.style.opacity = "0";
+
   loader.style.display = "none";
-  loader.querySelector(".spinner").style.opacity = 0;
 }
